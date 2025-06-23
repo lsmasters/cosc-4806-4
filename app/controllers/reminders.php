68b037747af3443e866cdd6c895f47356e4035c7
lsmasters <lmasters@algomau.ca>
2025-06-23 20:19:11 +0000

    Listed reminders in view and added a Create  Reminder link

--- app/controllers/reminders.php
+++ app/controllers/reminders.php
@@ -5,14 +5,15 @@ class Reminders extends Controller {
     public function index() {	
       $reminder = $this->model('Reminder');
       $list_of_reminders = $reminder->get_all_reminders();
-      // for testing only
+      // for testing only 
+      /*
         echo '<pre>';
       print_r($list_of_reminders);
       echo '</pre> ';
-      
       die;
-	    $this->view('reminders/index');
-	    die;
+      */
+	    $this->view('reminders/index',['reminders' => $list_of_reminders]);
+	    
     }
 
 }
--- app/views/reminders/index.php
+++ app/views/reminders/index.php
@@ -4,9 +4,15 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1>Reminders</h1>
+                <p><a href='/reminders/create'>Create Reminder</a></p>;
                 <p class="lead"> <?= date("F jS, Y"); ?></p>
             </div>
         </div>
     </div>
+    <?php
+        echo "<pre>";
+        print_r($data);
+        echo "</pre>";
+    ?>
 
     <?php require_once 'app/views/templates/footer.php' ?>
