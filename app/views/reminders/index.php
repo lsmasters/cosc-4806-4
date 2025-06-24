133c25146c0ea8c3c943f6e9e724470b42b899eb
lsmasters <lmasters@algomau.ca>
2025-06-24 19:29:22 +0000

    Reminders appear in reminder page

--- app/views/reminders/index.php
+++ app/views/reminders/index.php
@@ -5,17 +5,21 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1>Reminders</h1>
-                <p><a href='/reminders/create'>Create Reminder</a></p>;
+                
                 <p class="lead"> <?= date("F jS, Y"); ?></p>
+                <p><a href='/reminders/create'>Create Reminder</a></p>
             </div>
         </div>
     </div>
 </div>
+  
 
 <?php
-        echo "<pre>";
-        print_r($data);
-        echo "</pre>";
+    foreach ($data['reminders'] as $item) {
+        echo '<p>' . $item['subject'] . '</p>';
+    }
+        
 ?>
+
 
 <?php require_once 'app/views/templates/footer.php' ?>
